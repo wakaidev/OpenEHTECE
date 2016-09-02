0a92e52873b2f591a0d6368920f519d67f8fe4d5
Kelvin Fichter <kef50@case.edu>
2016-09-01 23:54:50 -0400

    Working on fl

--- Scripts/atclibs.py
+++ Scripts/atclibs.py
@@ -3,7 +3,6 @@ import timeit
 def format_phone(n):
 	return '({}) {}-{}'.format(n[:3], n[3:6], n[6:])
 
-
 def format_card(n):
 	return '{} {} {} {}'.format(n[:4], n[4:8], n[8:12], n[12:])
 
@@ -16,3 +15,14 @@ def tock():
     tock = timeit.default_timer()
     print(tock - tick)
     return tock
+
+def footsites_parse_size(size):
+    size_ = size
+
+    if size[-2:] != ".5":
+        size_ = size + '.0'
+        
+    if float(size) < 10:
+        size_ = '0' + size
+        
+    return (size_)
