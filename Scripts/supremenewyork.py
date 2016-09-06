7a6e2c99949e68ea6654782d7f790f34fc968da7
My-kal <burrisms@g.cofc.edu>
2016-09-05 21:33:31 -0400

    Update supremenewyork.py
    fixed syntax error

--- Scripts/supremenewyork.py
+++ Scripts/supremenewyork.py
@@ -22,6 +22,8 @@ size = 'OS'
 use_early_link = False
 
 early_link = ''
+
+
 # early_link = 'http://www.supremenewyork.com/shop/jackets/nzpacvjtk' #sold out
 # early_link = 'http://www.supremenewyork.com/shop/shirts/r1k32vjf4/sblz8csj2' # mult sizes
 # early_link = 'http://www.supremenewyork.com/shop/accessories/kcgevis8r/xiot9byq4' #one size
@@ -29,7 +31,7 @@ early_link = ''
 
 # Functions
 def product_page(url):
-	print('Finding matching products... \n', end='')
+	print('Finding matching products...', end='')
 	session = requests.Session()
 	session.headers.update({
 		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -41,26 +43,26 @@ def product_page(url):
 		'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
 		'DNT': '1'
 	})
-
+	
 	response = session.get(base_url + url)
 	soup = bs(response.text, 'html.parser')
-
+	
 	h1 = soup.find('h1', {'itemprop': 'name'})
-
+	
 	p = soup.find('p', {'itemprop': 'model'})
-
+	
 	match = []
-
+	
 	if h1 is not None and p is not None:
 		model = h1.string
 		style = p.string
-
+		
 		for keyword in keywords_model:
 			if keyword.title() in model:
 				match.append(1)
 			else:
 				match.append(0)
-
+		
 		# add to cart
 		if 0 not in match:
 			match = []
@@ -71,13 +73,13 @@ def product_page(url):
 					match.append(0)
 			if 0 not in match:
 				print('FOUND: ' + model + ' at ' + base_url + url)
-				add_to_cart(soup, base_url+url)
+				add_to_cart(soup, base_url + url)
 			else:
 				sys.exit('Sorry, couldnt find {} in {}'.format(model, style))
 
 
 def add_to_cart(soup, url):
-	product_name = soup.find('h1',{'itemprop': 'name'}).string
+	product_name = soup.find('h1', {'itemprop': 'name'}).string
 	print('Adding {} to cart...'.format(product_name))
 	session = requests.Session()
 	session.headers.update({
@@ -92,7 +94,7 @@ def add_to_cart(soup, url):
 	})
 	form = soup.find('form', {'action': re.compile('(?<=/shop/)(.*)(?=/add)')})
 	csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
-
+	
 	# find size
 	sold_out = soup.find('fieldset', {'id': 'add-remove-buttons'}).find('b')
 	if sold_out is not None:
@@ -105,7 +107,7 @@ def add_to_cart(soup, url):
 				size_value = soup.find('option', string=size.title())['value']
 			except:
 				sys.exit('Sorry, {} is sold out!'.format(size))
-
+	
 	if form is not None:
 		payload = {
 			'utf8': '✓',
@@ -123,9 +125,12 @@ def add_to_cart(soup, url):
 			'X-CSRF-Token': csrf_token,
 			'Accept-Encoding': 'gzip, deflate'
 		}
-
+		
 		session.post(base_url + form['action'], data=payload, headers=headers)
-		print('Added to cart!')
+		
+		response = session.get('http://www.supremenewyork.com/shop/cart')
+		soup = bs(response.text, 'html.parser')
+		
 		checkout(session)
 	else:
 		sys.exit('Sorry, product is sold out!')
@@ -136,7 +141,7 @@ def checkout(session):
 	response = session.get('https://www.supremenewyork.com/checkout')
 	soup = bs(response.text, 'html.parser')
 	form = soup.find('form', {'action': '/checkout'})
-
+	
 	csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
 	headers = {
 		'Accept': 'text/html, */*; q=0.01',
@@ -145,11 +150,11 @@ def checkout(session):
 		'Referer': 'https://www.supremenewyork.com/checkout',
 		'Accept-Encoding': 'gzip, deflate, sdch, br'
 	}
-
+	
 	country_abbrv = shipping_country_abbrv
 	if country_abbrv == 'US':
 		country_abbrv = 'USA'
-		
+	
 	if card_type.lower() in ['mastercard', 'master card', 'master']:
 		card_ = 'master'
 	elif card_type.lower() == 'visa':
@@ -160,69 +165,76 @@ def checkout(session):
 		sys.exit('You must be using a master, visa, or american express card')
 	
 	payload = {
-		'utf8': '✓',
-		'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
-		'order[billing_name]': first_name + ' ' + last_name,
-		'order[email]': email,
-		'order[tel]': format_phone(phone_number),
-		'order[billing_address]': shipping_address_1,
-		'order[billing_address_2]': shipping_apt_suite,
-		'order[billing_zip]': shipping_zip,
-		'order[billing_city]': shipping_city,
-		'order[billing_state]': shipping_state,
-		'order[billing_country]': country_abbrv,
-		'same_as_billing_address': '1',
-		'store_credit_id': '',
-		'credit_card[type]': card_,
-		'credit_card[cnb]': format_card(card_number),
-		'credit_card[month]': card_exp_month,
-		'credit_card[year]': card_exp_year,
-		'credit_card[vval]': card_cvv,
-		'order[terms]': '1',
-		'hpcvv': '',
-		'cnt': '2'
-	}
+			'utf8': '✓',
+			'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
+			'order[billing_name]': first_name + ' ' + last_name,
+			'order[email]': email,
+			'order[tel]': format_phone(phone_number),
+			'order[billing_address]': shipping_address_1,
+			'order[billing_address_2]': shipping_apt_suite,
+			'order[billing_zip]': shipping_zip,
+			'order[billing_city]': shipping_city,
+			'order[billing_state]': shipping_state,
+			'order[billing_country]': country_abbrv,
+			'same_as_billing_address': '1',
+			# 'store_address': '0',
+			'store_credit_id': '',
+			'credit_card[type]': card_,
+			'credit_card[cnb]': format_card(card_number),
+			'credit_card[month]': card_exp_month,
+			'credit_card[year]': card_exp_year,
+			'credit_card[vval]': card_cvv,
+			'order[terms]': '1',
+			'hpcvv': '',
+			'cnt': '2'
+		}
+	
 	response = session.get('https://www.supremenewyork.com/checkout.js', data=payload, headers=headers)
-
+	
 	payload = {
-		'utf8': '✓',
-		'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
-		'order[billing_name]': first_name + ' ' + last_name,
-		'order[email]': email,
-		'order[tel]': format_phone(phone_number),
-		'order[billing_address]': shipping_address_1,
-		'order[billing_address_2]': shipping_apt_suite,
-		'order[billing_zip]': shipping_zip,
-		'order[billing_city]': shipping_city,
-		'order[billing_state]': shipping_state_abbrv,
-		'order[billing_country]': country_abbrv,
-		'same_as_billing_address': '1',
-		'store_credit_id': '',
-		'credit_card[type]': card_type,
-		'credit_card[cnb]': format_card(card_number),
-		'credit_card[month]': card_exp_month,
-		'credit_card[year]': card_exp_year,
-		'credit_card[vval]': card_cvv,
-		'order[terms]': '1',
-		'hpcvv': ''
-	}
+			'utf8': '✓',
+			'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
+			'order[billing_name]': first_name + ' ' + last_name,
+			'order[email]': email,
+			'order[tel]': format_phone(phone_number),
+			'order[billing_address]': shipping_address_1,
+			'order[billing_address_2]': shipping_apt_suite,
+			'order[billing_zip]': shipping_zip,
+			'order[billing_city]': shipping_city,
+			'order[billing_state]': shipping_state_abbrv,
+			'order[billing_country]': country_abbrv,
+			'same_as_billing_address': '1',
+			'store_credit_id': '',
+			'store_address': '1',
+			'credit_card[type]': card_type,
+			'credit_card[cnb]': format_card(card_number),
+			'credit_card[month]': card_exp_month,
+			'credit_card[year]': card_exp_year,
+			'credit_card[vval]': card_cvv,
+			'order[terms]': '1',
+			'hpcvv': ''
+		}
+	
 	headers = {
 		'Origin': 'https://www.supremenewyork.com',
 		'Content-Type': 'application/x-www-form-urlencoded',
 		'Referer': 'https://www.supremenewyork.com/checkout',
 		'Accept-Encoding': 'gzip, deflate, br'
 	}
-
+	
 	response = session.post('https://www.supremenewyork.com/checkout', data=payload, headers=headers)
-
 	if 'Your order has been submitted' in response.text:
-		print('Checkout was successful!')
-		sys.exit(0)
+		print('Checkout was successful, check for a confirmation email!')
 	else:
-		soup = bs(response.text, 'html.parser')
-		print(soup.find('p').text)
-		sys.exit(0)
-
+		try:
+			soup = bs(response.text, 'html.parser')
+			error_msg = soup.find('div', {'class': 'errors'})
+			if error_msg is None:
+				print(soup.find('p').text)
+			else:
+				print('\n' + 'ERROR: ' + error_msg)
+		except:
+			print('Checkout failed')
 
 # Main
 tick()
@@ -261,7 +273,6 @@ else:
 				if product_link not in links_by_keyword1:
 					links_by_keyword1.append(link['href'])
 	pool1 = ThreadPool(len(links_by_keyword1))
-	print(links_by_keyword1)
 	result1 = pool1.map(product_page, links_by_keyword1)
 
 tock()  # runtime
