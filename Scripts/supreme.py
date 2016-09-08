9507c78513dd609230a3d29020e8fd9375c5d40a
My-kal <burrisms@g.cofc.edu>
2016-09-07 23:41:23 -0400

    Update and rename supreme-us.py to supreme.py
    Ability to add multiple products to cart
    Supports US and EU

--- Scripts/supreme.py
+++ Scripts/supreme.py
@@ -6,13 +6,6 @@ import re
 from getconf import *
 from atclibs import *
 
-# TODO: combine with supreme-eu; support all countries in eu
-
-# early_link = 'http://www.supremenewyork.com/shop/jackets/nzpacvjtk' #sold out
-# early_link = 'http://www.supremenewyork.com/shop/shoes/cvn1zj4ms' # mult sizes
-# early_link = 'http://www.supremenewyork.com/shop/accessories/kcgevis8r/xiot9byq4' #one size
-
-
 # Constants
 base_url = 'http://www.supremenewyork.com'
 
@@ -24,10 +17,11 @@ keyword_model = []
 keyword_style = []
 sizes = []
 
+keyword = True
 checkout_qty = int(input('How many products would you like to checkout? '))
 
-for counter, product in enumerate(range(checkout_qty)):
-	use_early_link = input('\n' + 'Do you have an early link for product ' + str(counter + 1) + '? ')
+for input_counter, product in enumerate(range(checkout_qty)):
+	use_early_link = input('\n' + 'Do you have an early link for product ' + str(input_counter + 1) + '? [y/n] ')
 	if use_early_link.lower() in ['y', 'yes']:
 		link = input('Enter the link: ').strip()
 		early_links.append(link)
@@ -37,16 +31,19 @@ for counter, product in enumerate(range(checkout_qty)):
 		keyword_model.append(False)
 		keyword_style.append(False)
 	else:
-		if counter == 0:
+		if keyword:
 			print('\n' + 'Example of a keyword search: ')
 			print('Category: accessories')
 			print('Model: hanes, socks')
 			print('Style: white', '\n')
+			keyword = False
 		
 		category = input('Enter category keyword: ').strip()
 		model = input('Enter model keywords seperated by commas: ').strip()
 		style = input('Enter style (color) keyword: ').strip()
 		
+		if category == 'tops/sweaters':
+			category = 'tops_sweaters'
 		keyword_category.append(category)
 		keyword_model.append(model.split(','))
 		keyword_style.append(style)
@@ -54,29 +51,68 @@ for counter, product in enumerate(range(checkout_qty)):
 		link_or_keyword.append(False)
 		early_links.append(False)
 	
-	if counter == 0:
+	if input_counter == 0:
 		print('\n' + 'Valid Clothing/Accessory Sizes: Small, Medium, Large, XLarge, OS')
 		print('If there is only one size, enter OS', '\n')
 	
 	size = input('Size: ').strip()
 	sizes.append(size)
-#
-# print(keyword_category)
-# print(keyword_model)
-# print(keyword_style)
-# print(sizes)
 
-country_abbrv = shipping_country_abbrv
-if country_abbrv == 'US':
-	country_abbrv = 'USA'
+country_lookup = {
+	'United States': 'USA',
+	'United Kingdom': 'GB',
+	'Austria': 'AT',
+	'Belarus': 'BY',
+	'Belgium': 'BE',
+	'Bulgaria': 'BG',
+	'Croatia': 'HR',
+	'Czech Republic': 'CZ',
+	'Demark': 'DK',
+	'Estonia': 'EE',
+	'Finland': 'FI',
+	'France': 'FR',
+	'Germany': 'DE',
+	'Greece': 'GR',
+	'Hungary': 'HU',
+	'Iceland': 'IS',
+	'Ireland': 'IE',
+	'Italy': 'IT',
+	'Lativa': 'LV',
+	'Lithuania': 'LT',
+	'Luxembourg': 'LU',
+	'Monaco': 'MC',
+	'Netherlands': 'NL',
+	'Northern Ireland': 'NB',
+	'Norway': 'NO',
+	'Poland': 'PO',
+	'Portugal': 'PT',
+	'Romania': 'RO',
+	'Russia': 'RU',
+	'Slovakia': 'SK',
+	'Slovenia': 'SI',
+	'Spain': 'ES',
+	'Sweden': 'SE',
+	'Switzerland': 'CH',
+	'Turkey': 'TR'
+}
+
+if shipping_country not in country_lookup:
+	sys.exit('Sorry, Supreme doesnt ship to your country')
+if shipping_country is not 'United States':
+	eu = True
+else:
+	eu = False
+	
 if card_type.lower() in ['mastercard', 'master card', 'master']:
 	card_ = 'master'
 elif card_type.lower() == 'visa':
 	card_ = 'visa'
 elif card_type.lower() == 'american express':
 	card_ = 'american_express'
+elif card_type.lower() == 'solo' and eu is True:
+		card_ = 'solo'
 else:
-	sys.exit('You must use a master, visa, or american express card')
+	sys.exit('You must use a master, visa, solo (EU only) or an american express card')
 
 
 # Functions
@@ -162,6 +198,10 @@ def add_to_cart(session, soup, url):
 	sold_out = soup.find('fieldset', {'id': 'add-remove-buttons'}).find('b')
 	if sold_out is not None:
 		print('Sorry, product is sold out!')
+		if counter == checkout_qty - 1:
+			sys.exit(0)
+		else:
+			return
 	else:
 		if sizes[counter].upper() == 'OS':
 			size_value = form.find('input', {'id': 'size'})['value']
@@ -170,6 +210,10 @@ def add_to_cart(session, soup, url):
 				size_value = form.find('option', string=sizes[counter].title())['value']
 			except:
 				print('Sorry, {} is sold out!'.format(sizes[counter]))
+				if counter == checkout_qty - 1:
+					sys.exit(0)
+				else:
+					return
 	
 	if form is not None:
 		payload = {
@@ -178,6 +222,9 @@ def add_to_cart(session, soup, url):
 			'size': size_value,
 			'commit': 'add to cart'
 		}
+		if eu:
+			payload['commit'] = 'add to basket'
+			
 		headers = {
 			'Accept': '*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript',
 			'Origin': 'http://www.supremenewyork.com',
@@ -219,7 +266,7 @@ def checkout(session):
 		'order[billing_zip]': shipping_zip,
 		'order[billing_city]': shipping_city,
 		'order[billing_state]': shipping_state,
-		'order[billing_country]': country_abbrv,
+		'order[billing_country]': shipping_country_abbrv,
 		'same_as_billing_address': '1',
 		'store_credit_id': '',
 		'credit_card[type]': card_,
@@ -229,33 +276,28 @@ def checkout(session):
 		'credit_card[vval]': card_cvv,
 		'order[terms]': '1',
 		'hpcvv': '',
-		'cnt': '2'
+		'cnt': '1'
 	}
+	
+	checkout_payload = payload.copy()
+	checkout_payload['order[tel]'] = format_phone(phone_number)
+
+	if eu:
+		payload['order[tel]'] = phone_number
+		payload['order[billing_address_2]'] = shipping_address_2
+		payload['order[billing_address_3]'] = shipping_apt_suite
+		del payload['store_credit_id']
+		del payload['order[billing_state]']
 	
 	session.get('https://www.supremenewyork.com/checkout.js', data=payload, headers=headers)
 	
-	payload = {
-		'utf8': '✓',
-		'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
-		'order[billing_name]': first_name + ' ' + last_name,
-		'order[email]': email,
-		'order[tel]': format_phone(phone_number),
-		'order[billing_address]': shipping_address_1,
-		'order[billing_address_2]': shipping_apt_suite,
-		'order[billing_zip]': shipping_zip,
-		'order[billing_city]': shipping_city,
-		'order[billing_state]': shipping_state_abbrv,
-		'order[billing_country]': country_abbrv,
-		'same_as_billing_address': '1',
-		'store_credit_id': '',
-		'credit_card[type]': card_type,
-		'credit_card[cnb]': format_card(card_number),
-		'credit_card[month]': card_exp_month,
-		'credit_card[year]': card_exp_year,
-		'credit_card[vval]': card_cvv,
-		'order[terms]': '1',
-		'hpcvv': ''
-	}
+	if eu:
+		
+		checkout_payload['order[billing_address_2]'] = shipping_address_2
+		checkout_payload['order[billing_address_3]'] = shipping_apt_suite
+		del checkout_payload['order[billing_state]']
+	del checkout_payload['cnt']
+	
 	headers = {
 		'Origin': 'https://www.supremenewyork.com',
 		'Content-Type': 'application/x-www-form-urlencoded',
@@ -263,17 +305,18 @@ def checkout(session):
 		'Accept-Encoding': 'gzip, deflate, br'
 	}
 	
-	response = session.post('https://www.supremenewyork.com/checkout', data=payload, headers=headers)
+	response = session.post('https://www.supremenewyork.com/checkout', data=checkout_payload, headers=headers)
 	
 	if 'Your order has been submitted' in response.text:
 		print('Checkout was successful, check for a confirmation email!')
 	else:
 		soup = bs(response.text, 'html.parser')
-		try:
-			for msg in soup.find_all('div', {'class': 'errors'}):
-				print('\n' + 'ERROR: ' + msg.string)
-		except:
+		error_container = soup.find_all('div', {'class': 'errors'})
+		if not error_container:
 			print(soup.find('p').text)
+		else:
+			for error in error_container:
+				print('ERROR: ' + error.string)
 
 
 # Main
@@ -308,7 +351,10 @@ for counter, i in enumerate(link_or_keyword):
 			soup = bs(response1.text, 'html.parser')
 		except:
 			print('Unable to connect to site...')
-			continue
+			if counter == checkout_qty - 1:
+				sys.exit()
+			else:
+				continue
 		add_to_cart(checkout_session, soup, early_links[counter])
 	else:  # keyword search
 		try:
@@ -316,7 +362,10 @@ for counter, i in enumerate(link_or_keyword):
 			response1 = search_session.get(url)
 		except:
 			print('Unable to connect to site...')
-			continue
+			if counter == checkout_qty - 1:
+				sys.exit()
+			else:
+				continue
 		
 		soup1 = bs(response1.text, 'html.parser')
 		links1 = soup1.find_all('a', href=True)
@@ -324,8 +373,8 @@ for counter, i in enumerate(link_or_keyword):
 		basura = ['http://www.supremenewyork.com/shop/faq', 'https://www.supremenewyork.com/checkout',
 		          'http://www.supremenewyork.com/shop']
 		basura += ['http://www.supremenewyork.com/shop/terms', 'http://www.supremenewyork.com/shop/sizing',
-		           'http://www.supremenewyork.com/shop/all']
-		basura += ['/shop/all', '/shop/new', '/shop/cart']
+		           'http://www.supremenewyork.com/shop/all', 'http://www.supremenewyork.com/shop/shipping']
+		basura += ['/shop/all', '/shop/new', '/shop/cart', '/shop/terms']
 		
 		links_by_keyword1 = []
 		for link in links1:
